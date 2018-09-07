defa13b0c1179bd45ca5b30577679a951ed5df9b
synergy2411 <synergy2411@outlook.com>
2018-09-07 12:31:38 +0530

    Library page added

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,25 +1,17 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Category } from './../../data/category.interface';
+import { Component, OnInit } from '@angular/core';
+import quotes from '../../data/quotes';
 
-/**
- * Generated class for the LibraryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  quotesCollection : Category[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
+      ngOnInit(){
+        this.quotesCollection = quotes;
+      }
 
 }
